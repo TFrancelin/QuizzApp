f1075ea57dd8e1da1c9f5146eae31037efa93f59
TFrancelin <Ancelt65@gmail.com>
2022-03-16 09:51:24 +0800

    Fisrt Vue App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ctr">
+    <questions-component v-if="questionsAnswered < questions.length"></questions-component>
+    <result-component v-else></result-component>
+    <button class="reset-btn" type="button">Reset</button>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import QuestionsComponent from "./components/Questions.vue"
+import ResultComponent from "./components/Result.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    QuestionsComponent, 
+    ResultComponent,
+  },
+  data() {
+    return {
+      questionsAnswered: 0,
+      questions: [
+        {
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "Fish",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: false,
+            }
+          ]
+        },
+        {
+          q: "How many letters are in the word 'Banana'?",
+          answers: [
+            {
+              text: "5",
+              is_correct: false
+            },
+            {
+              text: "7",
+              is_correct: false,
+            },
+            {
+              text: "6",
+              is_correct: true,
+            }
+          ] 
+        },
+        {
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: true
+            },
+            {
+              text: "i",
+              is_correct: false
+            }
+          ]
+        }
+      ],
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        }
+      ]
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Questions.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="questions-str">
+        <div class="process">
+            <div class="bar"></div>
+            <div class="status">1 out of 3 questions answered</div>
+        </div>
+        <div class="single-question">
+            <div class="question">Sample Question</div>
+            <div class="answer">
+                <div class="answer">Sample Answer 1</div>
+                <div class="answer">Sample Answer 2</div>
+                <div class="answer">Sample Answer 3</div>
+                <div class="answer">Sample Answer 4</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+name: 'QuestionsComponent',
+data() {
+    return {
+        
+    }
+},
+}
+</script>
+<style lang="">
+    
+</style>
